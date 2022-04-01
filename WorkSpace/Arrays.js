76db03eca55f0913b0ee7d013199f68b323ad981
Freddy Marin <freddymarin.jpg@gmail.com>
2022-04-01 14:57:32 -0600

    Added Arrays.js, Math.js and arrayMethod.js

---
+++ WorkSpace/Arrays.js
@@ -0,0 +1,7 @@
+//Get the value "Volvo" from the cars array.
+const cars = ["Saab", "Volvo", "BMW"];
+let x = cars[1];
+//Change the first item of cars to "Ford".
+cars[0] = "Ford";
+//Alert the number of items in an array, using the correct Array property.
+alert(cars.length);
---
+++ WorkSpace/Math.js
@@ -0,0 +1,8 @@
+//Use the correct Math method to create a random number.
+let r = Math.random();
+//Use the correct Math method to return the largest number of 10 and 20.
+let x = Math.max(10, 20);
+//Use the correct Math method to round a number to the nearest integer.
+let y = Math.round(5.3);
+//Use the correct Math method to get the square root of 9.
+let z = Math.sqrt(9);
